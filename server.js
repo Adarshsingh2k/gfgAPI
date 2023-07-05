2586997cc9398e3f500c92e923148f516a87c60c
Adarsh <adarshsingh2k@gmail.com>
2023-07-05 19:56:37 +0530

    commit  2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const { getUserData } = require("./logic")
 
-app.get('/data/:userID', async (req, res) => {
+app.get('https://delicate-salmiakki-299bcd.netlify.app/data/:userID', async (req, res) => {
    
 
     const user = req.params.userID
