09bf70b94a72d07aafc4ae3a343a1f0bac2cc88e
Adarsh <adarshsingh2k@gmail.com>
2023-07-05 20:13:15 +0530

    commit  4

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const { getUserData } = require("./logic")
 
-app.get('https://gfgunofficial-api.onrender.com/data/:userID', async (req, res) => {
+app.get('/:userID', async (req, res) => {
    
 
     const user = req.params.userID
